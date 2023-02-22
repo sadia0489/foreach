e6cc753ecdcb35a51897be9cd1266040d7444c86
sadia0489 <125875159+sadia0489@users.noreply.github.com>
2023-02-22 17:18:52 +0600

    Add files via upload

---
+++ foreach/apps.js
@@ -0,0 +1,43 @@
+
+
+// for(var i=1;i<=10;i++){
+	// document.write(i);
+// }
+// var i=0;
+// while(i<=10){
+	
+	// document.write(i+'<br>')
+	// i++;
+
+// }
+
+
+// do{
+	
+	// document.write(i+'<br>');
+	// i++;
+// }while(i<=10);
+
+
+// let sname=['Jamal Miah','Kamal','Rana','Jabed','Rashed'];
+
+// sname.forEach(function(value,index,arr){
+	// document.write(value+'<br>')
+	// // document.write(value+'-'+index+' '+arr+'<br>');
+
+	
+	
+// });
+// var total=0;
+// var i=1;
+// for(i=1;i<=10;i++){
+	// total+=i;
+// }
+// document.write(total);
+
+try {
+	alert();
+} catch (error) {
+  console.error(error);
+ 
+}
